1372a589ad87e525628fcdfbda16b4d442a281ce
Amir Bilu <amirbilu@codota.com>
2021-08-12 23:58:01 +0300

    initial init

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup_args = dict(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=["jupyter_server>=1.6,<2"],
+    install_requires=["jupyter_server==1.6.4"],
     zip_safe=False,
     include_package_data=True,
     python_requires=">=3.6",
